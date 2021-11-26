ef43ef18c690757a8606cb3946a5dde188364491
Ugo Romi <ugo.romi@gmail.com>
2021-11-26 01:17:44 +0100

    Improved carousle and added staking box style

--- src/pages/TradingPage.tsx
+++ src/pages/TradingPage.tsx
@@ -2,12 +2,14 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import Carousel from 'react-multi-carousel';
+import { PlusIcon } from '@heroicons/react/outline';
 
 interface availableStaking {
   tokenName: string;
   tokenImage: string;
   tokenSymbol: string;
-  amountInStaking: number;
+  totalAmountInStaking: number;
+  userAmountInStaking: number;
   APYPercentage: number;
   totalEarned: number;
   totalBalance: number;
@@ -17,14 +19,13 @@ interface availableStaking {
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 6,
-    partialVisibilityGutter: 40,
-    slidesToSlide: 1,
+    items: 4,
+    partialVisibilityGutter: 30,
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
     items: 2,
-    partialVisibilityGutter: 50,
+    partialVisibilityGutter: 30,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -54,14 +55,18 @@ export default function TradingPage() {
     <div>
       <div>
         <Carousel
-          ssr
-          swipeable={true}
-          draggable={true}
           itemClass='mx-4'
           responsive={responsive}
+          slidesToSlide={1}
+          swipeable
+          draggable
+          infinite
         >
           {staking.map((token, i) => (
-            <div key={i} className='rounded-md border-2 border-purple-400 p-2'>
+            <div
+              key={i}
+              className='rounded-md border-2 border-purple-400 bg-white shadow-md p-2 h-full'
+            >
               <div className='flex items-center gap-4'>
                 <img
                   src={token.tokenImage}
@@ -75,7 +80,50 @@ export default function TradingPage() {
                   </span>
                 </div>
               </div>
-              <div className='flex items-center justify-between'></div>
+              <div className='flex items-center justify-between mt-4'>
+                {token.userAmountInStaking > 0 ? (
+                  <>
+                    <div className='text-xs w-2/3 flex justify-evenly'>
+                      <div>
+                        <p className='font-semibold'>Earned</p>
+                        <p>{token.totalEarned}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(token.totalEarned * token.tokenPriceUSD)}
+                        </p>
+                      </div>
+                      <div className='border-r' aria-hidden></div>
+                      <div>
+                        <p className='font-semibold'>Balance</p>
+                        <p>{token.totalBalance}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(token.totalBalance * token.tokenPriceUSD)}
+                        </p>
+                      </div>
+                    </div>
+                    <div className='flex w-1/3 justify-center h-full'>
+                      <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                        <PlusIcon className='w-5' />
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className='text-sm'>
+                      <p className='font-semibold'>Total staking</p>
+                      <p>{token.totalAmountInStaking}</p>
+                    </div>
+                    <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                      Stake now
+                    </button>
+                  </>
+                )}
+              </div>
             </div>
           ))}
         </Carousel>
