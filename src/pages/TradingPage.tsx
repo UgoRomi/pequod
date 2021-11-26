84cfff775a617620394034b8c784efdd40a886d4
Ugo Romi <ugo.romi@gmail.com>
2021-11-26 20:20:54 +0100

    Added search bar

--- src/pages/TradingPage.tsx
+++ src/pages/TradingPage.tsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import Carousel from 'react-multi-carousel';
-import { PlusIcon } from '@heroicons/react/outline';
+import { PlusIcon, SearchIcon } from '@heroicons/react/outline';
 
-interface availableStaking {
+interface AvailableStaking {
   tokenName: string;
   tokenImage: string;
   tokenSymbol: string;
@@ -14,6 +14,12 @@ interface availableStaking {
   totalEarned: number;
   totalBalance: number;
   tokenPriceUSD: number;
+}
+
+interface Token {
+  name: string;
+  address: string;
+  symbol: string;
 }
 
 const responsive = {
@@ -35,7 +41,10 @@ const responsive = {
 };
 
 export default function TradingPage() {
-  const [staking, setStaking] = useState<availableStaking[]>([]);
+  const [staking, setStaking] = useState<AvailableStaking[]>([]);
+  const [tokenSearch, setTokenSearch] = useState<string>('');
+  const [searchResults, setSearchResults] = useState<Token[]>([]);
+  const [searchFocused, setSearchFocused] = useState<boolean>(false);
 
   useEffect(() => {
     axios
@@ -51,9 +60,28 @@ export default function TradingPage() {
       });
   }, []);
 
+  useEffect(() => {
+    if (!tokenSearch) {
+      setSearchResults([]);
+      return;
+    }
+    const searchTokens = setTimeout(() => {
+      axios
+        .get(`http://localhost:3001/tokens/${tokenSearch}`)
+        .then((res) => {
+          setSearchResults(res.data);
+        })
+        .catch((_err) => {
+          toast.error('Error retrieving tokens, please retry');
+        });
+    }, 200);
+
+    return () => clearTimeout(searchTokens);
+  }, [tokenSearch]);
+
   return (
     <div>
-      <div>
+      <div className='flex flex-col gap-11'>
         <Carousel
           itemClass='mx-4'
           responsive={responsive}
@@ -127,6 +155,48 @@ export default function TradingPage() {
             </div>
           ))}
         </Carousel>
+        <div className='grid grid-rows-2 grid-cols-3 lg:bg-white lg:p-5 lg:border-2 lg:border-purple-400 lg:rounded-md'>
+          <div className='flex-1 flex flex-col col-span-3'>
+            <p className='font-bold m-auto mb-3 text-md text-gray-800'>
+              Search token
+            </p>
+            <form className='w-full flex justify-center md:ml-0'>
+              <label htmlFor='search-field' className='sr-only'>
+                Search
+              </label>
+              <div className='relative w-full text-gray-400 focus-within:text-gray-600 max-w-xl'>
+                <div className='absolute inset-y-0 left-2 flex items-center pointer-events-none'>
+                  <SearchIcon className='h-5 w-5' aria-hidden='true' />
+                </div>
+                <input
+                  id='search-field'
+                  className='block w-full h-full pl-10 pr-3 py-2 bg-white lg:bg-purple-50 text-gray-600 focus:outline-none focus:ring placeholder-gray-400 focus:placeholder-gray-400 sm:text-sm rounded-md'
+                  placeholder='0xe861....'
+                  type='search'
+                  name='search'
+                  value={tokenSearch}
+                  onChange={(e) => setTokenSearch(e.target.value)}
+                  onFocus={() => setSearchFocused(true)}
+                  onBlur={() => setSearchFocused(false)}
+                />
+                {searchResults?.length > 0 && searchFocused && (
+                  <div className='z-10 mt-1 p-3 w-full shadow-md rounded-md absolute bg-white'>
+                    {searchResults.map((token, i) => (
+                      <div className='cursor-pointer' key={token.address}>
+                        <span className='p-1 text-md text-gray-800 font-semibold'>
+                          {token.name} - ${token.symbol}
+                        </span>
+                        <span className='block text-sm text-gray-600 overflow-hidden'>
+                          {token.address}
+                        </span>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
