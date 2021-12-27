16196c1344b4afda783ef2df68bb8aaddd1077a6
Ugo Romi <ugo.romi@gmail.com>
2021-12-27 20:13:48 +0100

    Design Staking

--- src/pages/TradingPage.tsx
+++ src/pages/TradingPage.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import {
   CogIcon,
+  LockClosedIcon,
   MinusIcon,
   PlusIcon,
   SearchIcon,
@@ -21,6 +22,25 @@ import {
   useSwap,
 } from '../utils/contractsUtils';
 import { selectPequodApiInstance } from '../store/axiosInstancesSlice';
+import Carousel from 'react-multi-carousel';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    partialVisibilityGutter: 30,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    partialVisibilityGutter: 30,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1.5,
+    partialVisibilityGutter: 30,
+  },
+};
 
 export enum eventCallback {
   PURCHASE,
@@ -305,6 +325,121 @@ export default function TradingPage() {
     <>
       <div>
         <div className='flex flex-col gap-11'>
+          <Carousel
+            itemClass='mx-4'
+            responsive={responsive}
+            slidesToSlide={1}
+            swipeable
+            draggable
+            infinite
+          >
+            <div className='rounded-md border-2 border-purple-400 bg-white shadow-md p-2 h-full'>
+              <div className='flex items-center gap-4'>
+                <img
+                  src='https://upload.wikimedia.org/wikipedia/commons/5/51/Mr._Smiley_Face.svg'
+                  alt='WOT Logo'
+                  className='h-10'
+                />
+                <div>
+                  <p className='font-bold'>WOT</p>
+                  <span className='text-sm opacity-75'>APY - 20%</span>
+                </div>
+              </div>
+              <div className='flex items-center justify-between mt-4'>
+                {false ? (
+                  <>
+                    <div className='text-xs w-2/3 flex justify-evenly'>
+                      <div>
+                        <p className='font-semibold'>Earned</p>
+                        <p>{100000}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(100000 * 1)}
+                        </p>
+                      </div>
+                      <div className='border-r' aria-hidden></div>
+                      <div>
+                        <p className='font-semibold'>Balance</p>
+                        <p>{10000000}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(10000000 * 1)}
+                        </p>
+                      </div>
+                    </div>
+                    <div className='flex w-1/3 justify-center h-full'>
+                      <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                        <LockClosedIcon className='w-5' />
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <div className='w-full flex justify-center'>
+                    <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                      Stake now
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className='rounded-md border-2 border-purple-400 bg-white shadow-md p-2 h-full'>
+              <div className='flex items-center gap-4'>
+                <img
+                  src='https://upload.wikimedia.org/wikipedia/commons/5/51/Mr._Smiley_Face.svg'
+                  alt='WOT Logo'
+                  className='h-10'
+                />
+                <div>
+                  <p className='font-bold'>WOT</p>
+                  <span className='text-sm opacity-75'>APY - 20%</span>
+                </div>
+              </div>
+              <div className='flex items-center justify-between mt-4'>
+                {true ? (
+                  <>
+                    <div className='text-xs w-2/3 flex justify-evenly'>
+                      <div>
+                        <p className='font-semibold'>Earned</p>
+                        <p>{100000}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(100000 * 1)}
+                        </p>
+                      </div>
+                      <div className='border-r' aria-hidden></div>
+                      <div>
+                        <p className='font-semibold'>Balance</p>
+                        <p>{10000000}</p>
+                        <p>
+                          {new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                          }).format(10000000 * 1)}
+                        </p>
+                      </div>
+                    </div>
+                    <div className='flex w-1/3 justify-center h-full'>
+                      <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                        <LockClosedIcon className='w-5' />
+                      </button>
+                    </div>
+                  </>
+                ) : (
+                  <div className='w-full flex justify-center'>
+                    <button className='bg-purple-400 text-white font-bold py-2 px-4 rounded-md'>
+                      Stake now
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </Carousel>
           <div className='grid grid-rows-buy grid-cols-2 gap-y-8 xl:bg-white xl:dark:bg-gray-900 xl:p-5 xl:border-2 xl:border-purple-400 xl:rounded-md'>
             <div className='col-span-2 gap-2 xl:gap-0 grid grid-cols-buy'>
               <div className='flex-1 flex flex-col'>
